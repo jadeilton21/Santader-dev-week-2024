adcf3fd85e027ef83aacb04e80b6739dc82dcd2c
Jadeilton <jadeiltondm@gmail.com>
2024-04-28 19:51:09 -0300

    feat

---
+++ src/main/java/Dio/me/Santaderdevweek2024/SantaderDevWeek2024Application.java
@@ -0,0 +1,13 @@
+package Dio.me.Santaderdevweek2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantaderDevWeek2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantaderDevWeek2024Application.class, args);
+	}
+
+}
