b69305d8c74327dbc04feafa224894968c2d1af3
Jadeilton <jadeiltondm@gmail.com>
2024-05-01 11:27:59 -0300

    feat: Entity

--- src/main/java/Dio/me/Santaderdevweek2024/model/User.java
+++ src/main/java/Dio/me/Santaderdevweek2024/model/User.java
@@ -1,10 +1,16 @@
 package Dio.me.Santaderdevweek2024.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import java.util.List;
-
+@Entity(name = "tb_user")
 public class User {
-
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private String nome;
 
