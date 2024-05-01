39a96302bb8c428c43eb7723df2e6275e7b0dd86
Jadeilton <jadeiltondm@gmail.com>
2024-05-01 11:10:51 -0300

    feat: create arguments in User

--- src/main/java/Dio/me/Santaderdevweek2024/model/User.java
+++ src/main/java/Dio/me/Santaderdevweek2024/model/User.java
@@ -1,4 +1,20 @@
 package Dio.me.Santaderdevweek2024.model;
 
+import java.util.List;
+
 public class User {
+
+
+
+    private String nome;
+
+    private Accout accout;
+
+    private List<Facture> facture;
+
+    private Card card ;
+
+    private List<News> news;
+
+
 }
