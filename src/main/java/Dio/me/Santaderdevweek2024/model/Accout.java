5af110e4103d6b7075fa519bbf06331a4cefa68c
Jadeilton <jadeiltondm@gmail.com>
2024-05-01 11:10:27 -0300

    feat: create arguments in Accout

--- src/main/java/Dio/me/Santaderdevweek2024/model/Accout.java
+++ src/main/java/Dio/me/Santaderdevweek2024/model/Accout.java
@@ -1,4 +1,18 @@
 package Dio.me.Santaderdevweek2024.model;
 
+import java.math.BigDecimal;
+
 public class Accout {
+
+
+    private String numero;
+    private String agencia;
+    private BigDecimal balance;
+    private BigDecimal limite;
+
+
+
+
+
+
 }
