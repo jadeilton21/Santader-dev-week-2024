da81bdcc3d4ca360e6985b760d0b4908847059f3
Jadeilton <jadeiltondm@gmail.com>
2024-05-01 11:27:49 -0300

    feat: Entity

--- src/main/java/Dio/me/Santaderdevweek2024/model/Accout.java
+++ src/main/java/Dio/me/Santaderdevweek2024/model/Accout.java
@@ -1,10 +1,12 @@
 package Dio.me.Santaderdevweek2024.model;
 
-import java.math.BigDecimal;
+import jakarta.persistence.Entity;
 
+import java.math.BigDecimal;
+@Entity(name = "tb_Acout")
 public class Accout {
 
-
+    private Long id;
     private String numero;
     private String agencia;
     private BigDecimal balance;
