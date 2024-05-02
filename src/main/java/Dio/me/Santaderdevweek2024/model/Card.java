bfabee646591902c832753a93cc3a66b231ccf3a
Jadeilton <jadeiltondm@gmail.com>
2024-05-02 18:45:53 -0300

    feat: create abstract class Card

--- src/main/java/Dio/me/Santaderdevweek2024/model/Card.java
+++ src/main/java/Dio/me/Santaderdevweek2024/model/Card.java
@@ -1,15 +1,41 @@
 package Dio.me.Santaderdevweek2024.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 
 import java.math.BigDecimal;
 @Entity(name = "tb_card")
 public class Card {
 
-
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
     private String numero;
+    @Column(name = "available_limit", precision = 13, scale = 2)
     private BigDecimal limite;
 
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public BigDecimal getLimite() {
+        return limite;
+    }
+
+    public void setLimite(BigDecimal limite) {
+        this.limite = limite;
+    }
 }
