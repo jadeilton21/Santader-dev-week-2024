12398a3111559cf80232e03b55a219fea3751331
Jadeilton <jadeiltondm@gmail.com>
2024-05-02 19:00:17 -0300

    feat: add anotations

--- src/main/java/Dio/me/Santaderdevweek2024/model/Accout.java
+++ src/main/java/Dio/me/Santaderdevweek2024/model/Accout.java
@@ -1,15 +1,21 @@
 package Dio.me.Santaderdevweek2024.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.math.BigDecimal;
 @Entity(name = "tb_Acout")
 public class Accout {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(unique = true)
     private String numero;
     private String agencia;
+    @Column(precision = 13, scale = 2)
     private BigDecimal balance;
+
+    @Column(name = "additional_limit", precision = 13, scale = 2)
     private BigDecimal limite;
 
 
