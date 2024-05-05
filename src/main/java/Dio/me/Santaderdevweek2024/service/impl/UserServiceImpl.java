a922f1822a46a20b88d88ce17ef0c7aec2150867
Jadeilton <jadeiltondm@gmail.com>
2024-05-05 18:56:49 -0300

    feat: create class ServiceImpl

--- src/main/java/Dio/me/Santaderdevweek2024/service/impl/UserServiceImpl.java
+++ src/main/java/Dio/me/Santaderdevweek2024/service/impl/UserServiceImpl.java
@@ -1,8 +1,29 @@
 package Dio.me.Santaderdevweek2024.service.impl;
 
+import Dio.me.Santaderdevweek2024.model.User;
 import Dio.me.Santaderdevweek2024.service.UserService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(NoSuchElementException::new);
+    }
+
+    @Override
+    public User create(User userToCreate) {
+        if (userRepository.existsByAccountNumber(userToCreate.getAccount().getNumber())) {
+            throw new IllegalArgumentException("This Account number already exists.");
+        }
+        return userRepository.save(userToCreate);
+
+    }
 }
