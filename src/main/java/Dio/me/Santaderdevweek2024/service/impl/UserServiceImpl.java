a4e702bfdeb7417a5d3919e15097cc35c70d20d4
Jadeilton <jadeiltondm@gmail.com>
2024-05-05 18:35:26 -0300

    feat: create classe UserServiceImpl

--- src/main/java/Dio/me/Santaderdevweek2024/service/impl/UserServiceImpl.java
+++ src/main/java/Dio/me/Santaderdevweek2024/service/impl/UserServiceImpl.java
@@ -1,6 +1,8 @@
 package Dio.me.Santaderdevweek2024.service.impl;
 
 import Dio.me.Santaderdevweek2024.service.UserService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl implements UserService {
 }
