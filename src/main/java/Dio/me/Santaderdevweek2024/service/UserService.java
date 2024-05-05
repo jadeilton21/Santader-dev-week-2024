b5b0fad601ff6336c085abe0d06b008fd5607936
Jadeilton <jadeiltondm@gmail.com>
2024-05-05 18:57:04 -0300

    feat: methods in UserService

--- src/main/java/Dio/me/Santaderdevweek2024/service/UserService.java
+++ src/main/java/Dio/me/Santaderdevweek2024/service/UserService.java
@@ -1,8 +1,12 @@
 package Dio.me.Santaderdevweek2024.service;
 
 
+import Dio.me.Santaderdevweek2024.model.User;
 import org.springframework.stereotype.Service;
 
-@Service
+
 public interface UserService {
+    User findById(Long id);
+
+    User create(User userToCreate);
 }
