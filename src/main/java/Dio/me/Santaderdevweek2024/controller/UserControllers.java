9755a8e6a9886c10a8e7d21c19496f695846d542
Jadeilton <jadeiltondm@gmail.com>
2024-05-06 22:33:31 -0300

    feat: add class UserService

--- src/main/java/Dio/me/Santaderdevweek2024/controller/UserControllers.java
+++ src/main/java/Dio/me/Santaderdevweek2024/controller/UserControllers.java
@@ -1,9 +1,15 @@
 package Dio.me.Santaderdevweek2024.controller;
 
+import Dio.me.Santaderdevweek2024.service.UserService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
 public class UserControllers {
+
+    private UserService userService;
+
+
+
 }
