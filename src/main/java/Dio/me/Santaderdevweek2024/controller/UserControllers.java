979ed6400bbd1e17a0311e710ba7c95b4b62c120
Jadeilton <jadeiltondm@gmail.com>
2024-05-06 22:27:28 -0300

    feat: create annotations

--- src/main/java/Dio/me/Santaderdevweek2024/controller/UserControllers.java
+++ src/main/java/Dio/me/Santaderdevweek2024/controller/UserControllers.java
@@ -1,4 +1,9 @@
 package Dio.me.Santaderdevweek2024.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
 public class UserControllers {
 }
