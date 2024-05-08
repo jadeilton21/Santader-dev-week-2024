3ecbcaedfef3dcd4aad14069957f48d8f3617d1b
Jadeilton <jadeiltondm@gmail.com>
2024-05-07 22:37:57 -0300

    feat: create arguments in methods

--- src/main/java/Dio/me/Santaderdevweek2024/controller/UserControllers.java
+++ src/main/java/Dio/me/Santaderdevweek2024/controller/UserControllers.java
@@ -4,6 +4,9 @@ import Dio.me.Santaderdevweek2024.doMain.model.User;
 import Dio.me.Santaderdevweek2024.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/users")
@@ -24,8 +27,14 @@ public class UserControllers {
 
     }
 
-
+    @PostMapping
     private ResponseEntity<User> create(@RequestBody User userToCreate){
+        var userCreated = userService.create(userToCreate);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(userCreated.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(userCreated);
 
     }
 }
