c7ed1eb32f572215f0c8ea824dbd409cdbcbb494
Jadeilton <jadeiltondm@gmail.com>
2024-05-18 11:36:51 -0300

    feat: add dependency GlobalExceptionHandler

--- src/main/java/Dio/me/Santaderdevweek2024/controller/exception/GlobalExceptionHandler.java
+++ src/main/java/Dio/me/Santaderdevweek2024/controller/exception/GlobalExceptionHandler.java
@@ -1,4 +1,8 @@
 package Dio.me.Santaderdevweek2024.controller.exception;
 
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 }
