94dae3799d9985e7cb4cfd7aed2d98feb8dcc1a0
Jadeilton <jadeiltondm@gmail.com>
2024-05-18 22:07:24 -0300

    feat

--- src/main/java/Dio/me/Santaderdevweek2024/controller/exception/GlobalExceptionHandler.java
+++ src/main/java/Dio/me/Santaderdevweek2024/controller/exception/GlobalExceptionHandler.java
@@ -1,8 +1,14 @@
 package Dio.me.Santaderdevweek2024.controller.exception;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+
+    private final Logger logger  = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
 }
