cf6bfbce26ccfaa334bcaab0f2e4bb7960785986
Jadeilton <jadeiltondm@gmail.com>
2024-05-18 22:07:49 -0300

    feat

--- src/main/java/Dio/me/Santaderdevweek2024/controller/exception/GlobalExceptionHandler.java
+++ src/main/java/Dio/me/Santaderdevweek2024/controller/exception/GlobalExceptionHandler.java
@@ -3,6 +3,9 @@ package Dio.me.Santaderdevweek2024.controller.exception;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
@@ -10,5 +13,10 @@ public class GlobalExceptionHandler {
 
 
     private final Logger logger  = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleBusinessException(IllegalArgumentException businessException) {
+        return new ResponseEntity<>(businessException.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
+    }
 
 }
