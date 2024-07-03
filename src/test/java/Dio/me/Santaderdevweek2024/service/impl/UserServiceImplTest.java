966c6eed52dad28de7fa13ab7c7a2d83e095d62b
Jadeilton <jadeiltondm@gmail.com>
2024-07-03 09:06:09 -0300

    feat: create test findById

--- src/test/java/Dio/me/Santaderdevweek2024/service/impl/UserServiceImplTest.java
+++ src/test/java/Dio/me/Santaderdevweek2024/service/impl/UserServiceImplTest.java
@@ -5,8 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @SpringBootTest
@@ -26,5 +28,9 @@ class UserServiceImplTest {
         var response = mockMvc
                 .perform(post(""))
                 .andReturn().getResponse();
+
+
+        assertThat(response.getStatus())
+                .isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 }
