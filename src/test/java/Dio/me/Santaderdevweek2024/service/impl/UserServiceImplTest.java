7cacf418da7033d36f4d2c0e65aae7f993cae1b3
Jadeilton <jadeiltondm@gmail.com>
2024-07-03 09:04:11 -0300

    feat: create test findById

--- src/test/java/Dio/me/Santaderdevweek2024/service/impl/UserServiceImplTest.java
+++ src/test/java/Dio/me/Santaderdevweek2024/service/impl/UserServiceImplTest.java
@@ -1,10 +1,13 @@
 package Dio.me.Santaderdevweek2024.service.impl;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -16,6 +19,12 @@ class UserServiceImplTest {
 
 
     @Test
-    void findById() {
+    @DisplayName("Deve Devolver Código Http 404 quando informações forem invalidas...")
+    void findById() throws Exception{
+
+
+        var response = mockMvc
+                .perform(post(""))
+                .andReturn().getResponse();
     }
 }
