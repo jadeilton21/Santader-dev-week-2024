80121c0a4e9139501370259eeffec4b02cef5417
Jadeilton <jadeiltondm@gmail.com>
2024-07-04 15:56:52 -0300

    feat: add biblioteca MockMvc

--- src/test/java/Dio/me/Santaderdevweek2024/service/UserServiceTest.java
+++ src/test/java/Dio/me/Santaderdevweek2024/service/UserServiceTest.java
@@ -1,7 +1,9 @@
 package Dio.me.Santaderdevweek2024.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,4 +13,8 @@ import static org.junit.jupiter.api.Assertions.*;
 @AutoConfigureMockMvc
 class UserServiceTest {
 
+
+
+    @Autowired
+    private MockMvc mockMvc;
 }
