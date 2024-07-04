8948323529f397dd51dfae47e588c81bcbd2ebfc
Jadeilton <jadeiltondm@gmail.com>
2024-07-04 15:55:18 -0300

    feat: add anotations

--- src/test/java/Dio/me/Santaderdevweek2024/service/UserServiceTest.java
+++ src/test/java/Dio/me/Santaderdevweek2024/service/UserServiceTest.java
@@ -1,7 +1,14 @@
 package Dio.me.Santaderdevweek2024.service;
 
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
 class UserServiceTest {
 
 }
